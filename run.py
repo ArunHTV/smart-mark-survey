2959544291876d3d491e255b68126d800d101aff
ArunHTV <gonnaliveforever@live.com>
2023-03-22 16:20:05 +0000

    push test

--- run.py
+++ run.py
@@ -6,4 +6,9 @@ print("Hello, " + name + str(". Thank you for taking my quiz."))
 # Input for users age
 
 age = input("How old are you? ")
-print("Thank you for providing your age, " + age + str("."))
+print("Thank you for providing your age, " + age + str(". Hmm. Sorry to hear that."))
+
+# First question
+
+first = input("Who is the first person you think of when you hear the word 'wrestling'? ")
+print(first + str("...pff. What a casual."))
