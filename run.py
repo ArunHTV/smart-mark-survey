82c608befe1f6169515db96967e47eae096d5653
ArunHTV <gonnaliveforever@live.com>
2023-03-22 23:30:28 +0000

    add formatting and more questions

--- run.py
+++ run.py
@@ -1,19 +1,34 @@
 # Input for users name
-
+print("\n")
 name = input("What is your name? ")
+print("-" * 80)
 print("Hello, " + name + str(". Thank you for taking my quiz."))
-
+print("-" * 80)
 # Input for users age
 
 age = input("How old are you? ")
-print("Thank you for providing your age, " + age + str(". Hmm. Getting on a bit, aren't you?\n"))
+print("-" * 80)
+print(age + str(". Hmm. Getting on a bit, aren't you?"))
+print("-" * 80)
 
-# First question
+# Questions
 
-first = input("Who is the first person you think of when you hear the word 'wrestling'?")
-print(first + str("...pff. What a casual.\n"))
-
-# Second question
+first = input("Who's the first person you associate with wrestling? ")
+print("-" * 80)
+print(first + str("...pff. What a casual."))
+print("-" * 80)
 
 second = input("Who was your favourite wrestler growing up? ")
-print(second + str(". Not a bad pick, actually.\n"))
+print("-" * 80)
+print(second + str(". Not a bad pick, actually."))
+print("-" * 80)
+
+third = input("Who do you consider the greatest wrestler of all time? ")
+print("-" * 80)
+print("So you picked " + third + str(". You're kidding, right?"))
+print("-" * 80)
+
+fourth = input("Who is your favourite tag team of all time? ")
+print("-" * 80)
+print("Wow...*scoff* " + fourth + str(". What a mark."))
+print("-" * 80)
