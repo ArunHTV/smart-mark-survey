4c8eeb08a3eeed73bd218bebf46b8a5f53c5c814
ArunHTV <gonnaliveforever@live.com>
2023-03-22 20:27:01 +0000

    add line breaks

--- run.py
+++ run.py
@@ -6,9 +6,14 @@ print("Hello, " + name + str(". Thank you for taking my quiz."))
 # Input for users age
 
 age = input("How old are you? ")
-print("Thank you for providing your age, " + age + str(". Hmm. Sorry to hear that."))
+print("Thank you for providing your age, " + age + str(". Hmm. Getting on a bit, aren't you?\n"))
 
 # First question
 
-first = input("Who is the first person you think of when you hear the word 'wrestling'? ")
-print(first + str("...pff. What a casual."))
+first = input("Who is the first person you think of when you hear the word 'wrestling'?")
+print(first + str("...pff. What a casual.\n"))
+
+# Second question
+
+second = input("Who was your favourite wrestler growing up? ")
+print(second + str(". Not a bad pick, actually.\n"))
