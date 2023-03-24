394667f9e8076ea16c1a55b9c442f86b6dfa397a
ArunHTV <gonnaliveforever@live.com>
2023-03-24 00:04:28 +0000

    finish questions with print responses referring to users input

--- run.py
+++ run.py
@@ -1,14 +1,13 @@
-# Input for users name
+# Input for users name and age
 print("\n")
 name = input("What is your name? ")
 print("-" * 80)
 print("Hello, " + name + str(". Thank you for taking my quiz."))
 print("-" * 80)
-# Input for users age
 
 age = input("How old are you? ")
 print("-" * 80)
-print(age + str(". Hmm. Getting on a bit, aren't you?"))
+print(age + str(". Oh. Getting on a bit, aren't you?"))
 print("-" * 80)
 
 # Questions
@@ -31,4 +30,34 @@ print("-" * 80)
 fourth = input("Who is your favourite tag team of all time? ")
 print("-" * 80)
 print("Wow...*scoff* " + fourth + str(". What a mark."))
+print("-" * 80)
+
+fifth = input("What's your dream singles match that never happened? ")
+print("-" * 80)
+print(fifth + str(". I'm disappointed in your lack of imagination, ") + name)
+print("-" * 80)
+
+sixth = input("What's your dream singles match that could still happen? ")
+print("-" * 80)
+print("Hmm. " + sixth + str(". Not bad indeed."))
+print("-" * 80)
+
+seventh = input("Switching gears, who is your favourite manager? ")
+print("-" * 80)
+print(seventh + str(". A fine choice."))
+print("-" * 80)
+
+eighth = input("We're almost done. What's your favourite pay-per-view?. ")
+print("-" * 80)
+print("Strange, when I want to fall asleep I watch " + eighth + str("."))
+print("-" * 80)
+
+ninth = input("And, your favourite weekly wrestling show of all time? ")
+print("-" * 80)
+print(ninth + str(". I too like to waste hours of my life."))
+print("-" * 80)
+
+tenth = input("Lastly, who is your LEAST favourite wrestler of all time? ")
+print("-" * 80)
+print("Hahaha, " + tenth + str(". I love it. Way to end on a high."))
 print("-" * 80)
